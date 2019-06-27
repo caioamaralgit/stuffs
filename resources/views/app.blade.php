ed48f578f7a95ea6be3e6f1b11de50403d58319c
Caio Amaral <cbarreto_sud@hotmail.com>
2019-06-27 20:17:46 -0300

    [check-my-wedding] Removed unnecessary content
    Removed packages, files and references

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <title>My Wedding - Julia e Caio</title>
+
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, user-scalable=no">
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+        <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    </head>
+    <body>
+    </body>
+</html>
