073b69c2fcadaefd52e8703ef4aa4c9f3f99921b
Caio Amaral <cbarreto_sud@hotmail.com>
2019-06-27 20:30:25 -0300

    [check-my-wedding] Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,16 @@
         <title>My Wedding - Julia e Caio</title>
 
         <meta charset="UTF-8" />
+        <meta name="theme-color" content="#ffffff">
+        <meta name="msapplication-TileColor" content="#2b5797">
         <meta name="viewport" content="width=device-width, user-scalable=no">
-
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/icon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/icon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/icon/favicon-16x16.png') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+        
         <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
         <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
     </head>
